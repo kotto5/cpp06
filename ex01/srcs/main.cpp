525c39ee10498956571000a74916eff44e8c9385
dorakueYaritai <kotto555555@gmail.com>
2023-09-11 00:31:12 +0900

    adfs

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -12,4 +12,7 @@ int main()
     Data *ptr2 = Serializer::deserialize(rawPtr);
     std::cout << "DeserializedPtr: " << ptr2 << std::endl;
     std::cout << "DeserializedValue: " << ptr2->getData() << std::endl;
+    std::cout << "rawPtr == DeserializedPtr: " << (rawPtr == (uintptr_t)ptr2) << std::endl;
+    std::cout << "DataValue == DeserializedValue: " << (ptr->getData() == ptr2->getData()) << std::endl;
+    
 }
