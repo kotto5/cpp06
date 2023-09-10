eb68333059f920120d9348e1304ec8eb8d71c64b
dorakueYaritai <kotto555555@gmail.com>
2023-09-11 00:28:04 +0900

    test

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -4,77 +4,76 @@
 #include <iomanip>
 #include "ScalarConverter.hpp"
 
+#define PINK "\033[38;5;206m"
+#define GREEN "\033[38;5;46m"
+#define DEF "\033[0m"
+
+void    ExeConvert(const std::string &str)
+{
+    std::cout << PINK << "input:   [" << str << "]" << DEF << std::endl;
+    ScalarConverter::convert(str);
+    std::cout << "==========================================" << std::endl;
+}
+
 void    testCharBoundaryValue()
 {
-    std::cout << "test char boundary value ==================" << std::endl;
     std::stringstream ss;
 
-    std::cout << "char test: " << (int)std::numeric_limits<char>::max() << std::endl;
     int n = std::numeric_limits<char>::max();
     ss << n;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
     ss.str("");
     ss.clear();
 
-    std::cout << "char test: " << (int)std::numeric_limits<char>::max() + 1 << std::endl;
     n = std::numeric_limits<char>::max() + 1;
     ss << n;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
     ss.str("");
     ss.clear();
 
-    std::cout << "char test: " << (int)std::numeric_limits<char>::min() << std::endl;
     n = std::numeric_limits<char>::min();
     ss << n;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
     ss.str("");
     ss.clear();
 
-    std::cout << "char test: " << (int)std::numeric_limits<char>::min() - 1 << std::endl;
     n = (int)std::numeric_limits<char>::min() - 1;
     ss << n;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
     ss.str("");
     ss.clear();
 }
 
 void    testCharPrintableBoundaryValue()
 {
-    std::cout << "test char printable boundary value ==================" << std::endl;
     std::stringstream ss;
 
     char arg[2];
 
     arg[1] = '\0';
 
-    std::cout << "char test: " << 32 << std::endl;
     arg[0] = 32;
-    ScalarConverter::convert(arg);
+    ExeConvert(arg);
 
-    std::cout << "char test: " << 31 << std::endl;
     arg[0] = 31;
-    ScalarConverter::convert(arg);
+    ExeConvert(arg);
 
-    std::cout << "char test: " << 126 << std::endl;
     arg[0] = 126;
-    ScalarConverter::convert(arg);
+    ExeConvert(arg);
 
-    std::cout << "char test: " << 127 << std::endl;
     arg[0] = 127;
-    ScalarConverter::convert(arg);
+    ExeConvert(arg);
 }
 
 void    exeTestInt(long n)
 {
-    std::cout << "int boundary test: " << n << std::endl;
     std::stringstream ss;
     ss << n;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
 }
 
 void    testIntBoundaryValue()
 {
-    std::cout << "test int boundary value ==================" << std::endl;
 
     exeTestInt(std::numeric_limits<int>::max());
     exeTestInt(std::numeric_limits<int>::max() + 1L);
@@ -84,22 +83,19 @@ void    testIntBoundaryValue()
 
 void    exeTestFloat(double d)
 {
-    std::cout << "float boundary test: " << d << std::endl;
     std::stringstream ss;
     ss << d;
     ss << "f";
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
 }
 
 void    exeTestString(const std::string &str)
 {
-    std::cout << "test: [" << str << "]" <<  std::endl;
-    ScalarConverter::convert(str);
+    ExeConvert(str);
 }
 
 void    testFloatBoundaryValue()
 {
-    std::cout << "test float boundary value ==================" << std::endl;
 
     exeTestFloat(std::numeric_limits<float>::max());
     exeTestFloat(std::numeric_limits<float>::min());
@@ -110,15 +106,13 @@ void    testFloatBoundaryValue()
 
 void    exeTestDouble(double d)
 {
-    std::cout << "double boundary test: " << d << std::endl;
     std::stringstream ss;
     ss << d;
-    ScalarConverter::convert(ss.str());
+    ExeConvert(ss.str());
 }
 
 void    testDoubleBoundaryValue()
 {
-    std::cout << "test double boundary value ==================" << std::endl;
 
     exeTestDouble(std::numeric_limits<double>::max());
     exeTestDouble(std::numeric_limits<double>::min());
@@ -140,13 +134,13 @@ void    testDoubleBoundaryValue()
 
 int main(int argc, char **argv)
 {
-    for (int i = 1; i < argc; i++)
-        ScalarConverter::convert(argv[i]);
     testCharBoundaryValue();
     testCharPrintableBoundaryValue();
     testIntBoundaryValue();
     testFloatBoundaryValue();
     testDoubleBoundaryValue();
+    for (int i = 1; i < argc; i++)
+        ExeConvert(argv[i]);
 
     return (0);
 }
